1460da5d6550da25828206fdb5b3e19145403230
tonmoydevbd <tonmoydevbd@gmail.com>
2026-05-30 18:26:37 +0600

    added footer component.

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,6 +1,30 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { TbBrandInstagramFilled } from "react-icons/tb";
+import { Link } from "react-router";
+
 const Footer = () => {
   return (
-    <p>Footer section...</p>
+    <div className="bg-[#244D3F] text-white py-8">
+      <div className="text-center ">
+        <h2 className="text-6xl font-extrabold mt-12">KeenKeeper</h2>
+        <p className="my-6">Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+        <h3 className="text-xl font-medium mb-4">Social Links</h3>
+        <div className="flex justify-center gap-2 text-black">
+          <Link className="p-3 bg-white rounded-full" to={'www.facebook.com'}><FaFacebookSquare /></Link>
+          <Link className="p-3 bg-white rounded-full" to={'www.instagram.com'}><TbBrandInstagramFilled /></Link>
+          <Link className="p-3 bg-white rounded-full" to={'www.x.com'}><FaXTwitter /></Link>
+        </div>
+      </div>
+      <div className="max-w-277.5 mx-auto sm:flex justify-between items-center py-6">
+        <p>© 2026 KeenKeeper. All rights reserved.</p>
+        <div className="flex gap-8">
+          <Link to={'/'}>Privacy Policy</Link>
+          <Link to={'/'}>Terms of Service</Link>
+          <Link to={'/'}>Cookies</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
